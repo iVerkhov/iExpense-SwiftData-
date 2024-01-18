a1ef4ff87f1f006227ed458979a7c34bf38da3e7
Igor Verkhov <iVerkhov@icoud.com>
2024-01-19 00:21:22 +0300

    Nev version of the course

---
+++ iExpense/ExpenseList.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseList.swift
+//  iExpense
+//
+//  Created by Игорь Верхов on 18.01.2024.
+//
+
+import SwiftUI
+
+struct ExpenseList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseList()
+}
